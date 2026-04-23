06b37257aa3f8e6240c25022c55ace5664e2302c
Opilane TTHK <opilane@tthk.ee>
2026-04-23 14:53:14 +0300

    Update

--- University/University/Controllers/StudentController.cs
+++ University/University/Controllers/StudentController.cs
@@ -127,5 +127,37 @@ namespace University.Controllers
             }
             return View(vm);
         }
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var student = await _context.Students
+           .Include(s => s.Enrollments)
+                //Kui tahad uuesti objeti kasutada objekti sees, siis kasutad ThenInclude
+           .ThenInclude(e => e.Course)
+                //andmeid ei salvestata vähemällu ja ei jälgita
+           .AsNoTracking()
+                // Leiab esimese elemendi andmetes, mis on tingimuse välja toodud 
+           .FirstOrDefaultAsync(m => m.Id == id);
+
+            //kui student on null, siis on NotFound()
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+           
+            var vm = new StudentUpdateViewModel
+            { 
+                Id = student.Id,
+                FirstMidName = student.FirstMidName,
+                LastName = student.LastName,
+                EnrollmentDate = student.EnrollmentDate
+            };
+
+            //tuleb teha domaini modelist andmete ülekanne view modeli omasse
+            
+            return View(vm);
+        }
+      
     }
 }
