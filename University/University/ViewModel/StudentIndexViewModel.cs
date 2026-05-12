eeb43a9e54be4b288e1205cc5f65f89064bd314e
jelizaveta890-prog <jelizaveta890@gmail.com>
2026-05-12 14:06:10 +0300

    StudentIndexViewModel

--- University/University/ViewModel/StudentIndexViewModel.cs
+++ University/University/ViewModel/StudentIndexViewModel.cs
@@ -1,12 +1,21 @@
-﻿using University.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using University.Models;
 
 namespace University.ViewModel
 {
     public class StudentIndexViewModel
     {
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime EnrollmentDate { get; set; }
 
     }
