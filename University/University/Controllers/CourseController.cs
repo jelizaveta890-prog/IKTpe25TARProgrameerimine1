91204b3ea78cdac4d56db6da8e7752a5bd81a91d
Opilane TTHK <opilane@tthk.ee>
2026-05-26 15:55:16 +0300

    Course

---
+++ University/University/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using University.Data;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class CourseController : Controller
+    {
+        //on vaja kutsuda välja  University constructor 
+
+        private readonly UniversityContext _context;
+
+        public CourseController
+
+            (
+                 UniversityContext context
+            )
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Courses
+                  .Include(c => c.DepartmentId)
+                  .AsNoTracking()
+                  .ToListAsync();
+
+            return View(result);
+        }
+
+    }
+}
+
--- University/University/Models/Department.cs
+++ University/University/Models/Department.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace University.Models
 {
     public class Department
     {
+
+        [Key]
         public int DepartmentId { get; set; }
         public string Name { get; set; }
 
