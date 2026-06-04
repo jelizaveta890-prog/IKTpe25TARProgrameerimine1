9f175a66b9c92d877c6fb25e98303cca78fa9b85
jelizaveta890-prog <jelizaveta890@gmail.com>
2026-06-04 11:17:15 +0300

    CreateAndDetails

---
+++ University/University/Controllers/CourseController.cs
@@ -0,0 +1,156 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+using University.Models;
+using University.ViewModel.CourseVM;
+
+namespace University.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly UniversityContext _context;
+        public CourseController
+            (
+                UniversityContext context
+            )
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var course = _context.Courses
+                .Select(c => new CourseIndexViewModel
+                {
+                    CourseId = c.CourseId,
+                    Credits = c.Credits,
+                    Title = c.Title,
+                    DepartmentId = c.DepartmentId,
+                    Department = new CourseDepartmentIndexViewModel
+                    {
+                        DepartmentName = c.Departments.Name
+                    }
+                });
+
+            return View(course);
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vm = await _context.Courses
+                .Where(c => c.CourseId == id)
+                .Select(c => new CourseUpdateViewModel
+                {
+                    CourseId = c.CourseId,
+                    Credits = c.Credits,
+                    Title = c.Title,
+                    Department = new CourseDepartmentIndexViewModel
+                    {
+                        DepartmentName = c.Departments != null ? c.Departments.Name : null
+                    }
+                })
+                .FirstOrDefaultAsync();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CourseUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = new Course
+                {
+                    CourseId = vm.CourseId,
+                    Title = vm.Title,
+                    Credits = vm.Credits,
+                    Departments = new Department
+                    {
+                        Name = vm.Department.DepartmentName
+                    }
+                };
+
+                _context.Update(course);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            PopulateDepartmentDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CourseCreateViewModel vm)
+        {
+
+            var course = new Course
+            {
+                CourseId = vm.CourseId,
+                Title = vm.Title,
+                Credits = vm.Credits,
+                DepartmentId = vm.DepartmentId,
+            };
+
+            _context.Add(course);
+            await _context.SaveChangesAsync();
+
+            PopulateDepartmentDropDownList(course.DepartmentId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Departments)
+                .Where(c => c.CourseId == id)
+                .Select(c => new CourseDetailsViewModel
+                {
+                    CourseId = c.CourseId,
+                    Credits = c.Credits,
+                    Title = c.Title,
+                    DepartmentId = c.DepartmentId,
+                    Department = new CourseDepartmentIndexViewModel
+                    {
+                        DepartmentName = c.Departments.Name
+                    }
+                })
+                .FirstOrDefaultAsync();
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        private void PopulateDepartmentDropDownList(object selectedDepartment = null)
+        {
+            var departmentsQuery = _context.Departments
+                .OrderBy(d => d.Name)
+                .GroupBy(d => d.Name)
+                .Select(g => g.First());
+
+            ViewBag.DepartmentId = new SelectList(departmentsQuery
+                .AsNoTracking(), "DepartmentId", "Name", selectedDepartment);
+        }
+    }
+}
